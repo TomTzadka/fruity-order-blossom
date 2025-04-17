bdc398629d082c8c46cbc326ec609ad97e987e45
TomTzadka <tomtzadka@gmail.com>
2025-04-17 21:26:40 +0300

    echo start

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+echo "🐳 Start..."
 
 echo "🐳 Docker Containers Down..."
 sudo docker compose down -v
 
 echo "🐳 Building Docker Containers..."
-docker compose build
+sudo docker compose build
 
 echo "🚀 Starting Docker Compose..."
-docker compose up
+sudo docker compose up
